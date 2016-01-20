77edb11c540dcaea90107f138837051bbcdbc712
darwin <oleg.umarov@gmail.com>
2016-01-20 17:28:31 -0600

    setup basic structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
 //create a new component. This component should produce some html
+// const App = function() {
+//   return <div>Hi!</div>;
+// }
 
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return <div>Hi!</div>
 }
 
 
 //take this component's generate HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
